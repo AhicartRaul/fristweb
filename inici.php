e1f4bea36799c4326775950d0dad5558a774eba5
AhicartRaul <66217467+AhicartRaul@users.noreply.github.com>
2020-09-28 12:36:26 +0200

    Iniciar repositorio

---
+++ inici.php
@@ -0,0 +1,8 @@
+﻿<?php
+	echo "Hola mòn";
+	// mostrem la informació continguda
+	echo "<hr>";
+	// mostrem la IP que recuperem de l'array $_SERVER[]
+	echo "Accés des de l'adreça IP: <B>".$_SERVER['REMOTE_ADDR']."</B>";
+	echo "<BR>Script que s'està executant: <B>".$_SERVER['PHP_SELF']."</B>";
+ ?>
