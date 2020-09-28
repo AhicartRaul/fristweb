8514542d5be8e773f3b6c78a0f2819e1da89f700
AhicartRaul <aluahi828@ieselcaminas.org>
2020-09-28 13:04:50 +0200

    Eliminar basura final fichero

--- inici.php
+++ inici.php
@@ -4,5 +4,4 @@
 	echo "<hr>";
 	// mostrem la IP que recuperem de l'array $_SERVER[]
 	echo "Accés des de l'adreça IP: <B>".$_SERVER['REMOTE_ADDR']."</B>";
-	echo "<BR>Script que s'està executant: <B>".$_SERVER['PHP_SELF']."</B>";
  ?>
